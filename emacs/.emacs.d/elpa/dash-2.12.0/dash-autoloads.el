3c1dae7ea05d5399d9f2c9c9821a019faa3225c2
zackn9ne <zackn9ne@gmail.com>
2016-12-28 23:51:46 -0500

    mutt only

--- emacs/.emacs.d/elpa/dash-2.12.0/dash-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; dash-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("dash-functional.el" "dash-pkg.el" "dash.el")
-;;;;;;  (22610 49831 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dash-autoloads.el ends here
--- emacs/.emacs.d/elpa/exec-path-from-shell-0/exec-path-from-shell-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "exec-path-from-shell" "0" "Get environment variables such as $PATH from the shell" 'nil :authors '(("Steve Purcell" . "steve@sanityinc.com")) :maintainer '("Steve Purcell" . "steve@sanityinc.com") :url "https://github.com/purcell/exec-path-from-shell")
