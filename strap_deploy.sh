880ad963f01fbcea9721df419dd5d21888247e22
zackn9ne <zackn9ne@gmail.com>
2016-12-29 00:35:56 -0500

    with readme

--- strap_deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cp -r ~/.emacs.d ~/.emacs.d.bak
-echo "backup of .emacs.d made"
-
-cp -r  ~/bootstrap/emacs/.emacs.d ~/
--- strap_down.sh
+++ strap_down.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cp -r ~/.mutt* ~/bootstrap/muttfiles
-cd ~/bootstrap
+cp -r ~/.mutt* ~/bootstrap_mutt/muttfiles
+cd ~/bootstrap_mutt
 git commit -am "$date automated from strap_down"
