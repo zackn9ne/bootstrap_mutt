e7a2227188fdb06b4f61786866b1e708805ae1e1
zackn9ne <zackn9ne@gmail.com>
2017-12-12 21:57:56 -0500

    Update strap_down.sh

--- strap_down.sh
+++ strap_down.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-cp -r ~/.mutt* ~/bootstrap_mutt/muttfiles
-cd ~/bootstrap_mutt
-git commit -am "$date automated from strap_down"
+echo "this will overwrite your mutt config do you want to proceed"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) cp -r ~/bootstrap_mutt/muttfiles/.mutt* ~/; break;;
+        No ) exit;;
+    esac
+done
